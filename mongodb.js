494ecbcdc2e84ad4e6302d6361212ea1007de378
Ivan Gorovoy <ivangorovoyyy@gmail.com>
2019-09-24 21:43:34 +0700

    finish 10 chapter

--- mongodb.js
+++ mongodb.js
@@ -19,39 +19,4 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").findOne(
-    //   { _id: ObjectID("5d8a095e525ce554f0e2560d") },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log("unable to fetch");
-    //     }
-
-    //     console.log(user);
-    //   }
-    // );
-
-    // db.collection("users")
-    //   .find({ age: 27 })
-    //   .toArray((error, users) => {
-    //     console.log(users);
-    //   });
-
-    // db.collection("users")
-    //   .find({ age: 27 })
-    //   .count((error, count) => {
-    //     console.log(count);
-    //   });
-
-    db.collection("tasks").findOne(
-      { _id: ObjectID("5d8a05a7cded4d4efe41ae47") },
-      (error, task) => {
-        console.log(task);
-      }
-    );
-    db.collection("tasks")
-      .find({ completed: false })
-      .toArray((error, tasks) => {
-        console.log(tasks);
-      });
-  }
 );
