677e5c5536af30379631488118779ec38d3ef739
Ivan <ivangorovoi1@gmail.com>
2019-09-30 20:08:08 +0700

    add bcrypt and jwt

--- src/index.js
+++ src/index.js
@@ -14,3 +14,17 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+const jwt = require("jsonwebtoken");
+
+const myFunction = () => {
+  const token = jwt.sign({ _id: "abc123" }, "thisismynewcourse", {
+    expiresIn: "7 days"
+  });
+  console.log(token);
+
+  const isVerify = jwt.verify(token, "thisismynewcourse");
+  console.log(isVerify);
+};
+
+myFunction();
