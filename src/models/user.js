4ef1a4c859f2ff0b4a9e39f84e380c1504f08bbb
Ivan <ivangorovoi1@gmail.com>
2019-10-01 16:18:39 +0700

    done chapter 12

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const bcryptjs = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -51,6 +52,12 @@ const userSchema = new mongoose.Schema({
   ]
 });
 
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner"
+});
+
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
@@ -94,6 +101,13 @@ userSchema.pre("save", async function(next) {
     user.password = await bcryptjs.hash(user.password, 8);
   }
 
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre("remove", async function(next) {
+  const user = this;
+  Task.deleteMany({ owner: user._id });
   next();
 });
 
