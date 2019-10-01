4f50528a3e2281d204d24e0a63e9c432543c51b4
Ivan <ivangorovoi1@gmail.com>
2019-10-01 17:45:00 +0700

    add sorting, pagination

--- src/index.js
+++ src/index.js
@@ -14,17 +14,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task.findById("5d9310b51e835b5e0524d907");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-
-  const user = await User.findById("5d930fd89d0c5959bc19a8fc");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
-main();
